9e25aa16f596d84d918b191329271c71e4a20be2
Kestas <k33370@gmail.com>
2023-02-08 14:12:32 +0200

    Testinis nulinis commit

--- Karolina.java
+++ Karolina.java
@@ -1,6 +1,6 @@
 public class Karolina {
     public static void main(String[] args) {
-        System.out.println("Pypiri papyri poo - kazka as cia hackinuuuu");
+        //system.out.println("Pypiri papyri poo - kazka as cia hackinuuuu");
         assert true;
     }
 }
